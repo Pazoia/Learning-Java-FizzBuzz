ad0e2684ae64d1ebff0b1b2259f4cfcbb39e1341
Paulo Azoia <pazoia.gomes@gmail.com>
2021-08-24 13:49:25 +0100

    Four tests added and passing:
    return Buzz When Number Is Divisable By Five
    return FizzBuzz When Number Is Divisable By Three And Five
    return Number When Number Is Not Divisable By Three Or Five
    return String Up To 100 With FizzBuzz Applied To Numbers Three And Five

--- src/main/java/Fizzbuzz.java
+++ src/main/java/Fizzbuzz.java
@@ -1,6 +1,8 @@
 public class Fizzbuzz {
     public String convert(Integer num) {
-        if (num % 3 == 0) {
+        if (num % 3 == 0 && num % 5 == 0) {
+            return "FizzBuzz";
+        } else if (num % 3 == 0) {
             return "Fizz";
         } else if (num % 5 == 0) {
             return "Buzz";
--- src/test/java/FizzbuzzTest.java
+++ src/test/java/FizzbuzzTest.java
@@ -31,4 +31,25 @@ public class FizzbuzzTest {
         String result = subject.convert(5);
         assertEquals("Buzz", result);
     }
+
+    @Test
+    void returnFizzBuzzWhenNumberIsDivisableByThreeAndFive() {
+        Fizzbuzz subject = new Fizzbuzz();
+        String result = subject.convert(15);
+        assertEquals("FizzBuzz", result);
+    }
+
+    @Test
+    void returnNumberWhenNumberIsNotDivisableByThreeOrFive() {
+        Fizzbuzz subject = new Fizzbuzz();
+        String result = subject.convert(4);
+        assertEquals("4", result);
+    }
+
+    @Test
+    void returnStringUpTo100WithFizzBuzzAppliedToThreeAndFive() {
+        Fizzbuzz subject = new Fizzbuzz();
+        String result = subject.generate(100);
+        assertEquals("1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz, 16, 17, Fizz, 19, Buzz, Fizz, 22, 23, Fizz, Buzz, 26, Fizz, 28, 29, FizzBuzz, 31, 32, Fizz, 34, Buzz, Fizz, 37, 38, Fizz, Buzz, 41, Fizz, 43, 44, FizzBuzz, 46, 47, Fizz, 49, Buzz, Fizz, 52, 53, Fizz, Buzz, 56, Fizz, 58, 59, FizzBuzz, 61, 62, Fizz, 64, Buzz, Fizz, 67, 68, Fizz, Buzz, 71, Fizz, 73, 74, FizzBuzz, 76, 77, Fizz, 79, Buzz, Fizz, 82, 83, Fizz, Buzz, 86, Fizz, 88, 89, FizzBuzz, 91, 92, Fizz, 94, Buzz, Fizz, 97, 98, Fizz, Buzz", result);
+    }
 }
